6691039aead244f3f5e0b5ac3a81e52516dd98ac
FikoReborn <fikosan@gmail.com>
2018-04-27 15:32:28 -0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,36 @@
+$(document).ready(function() {
 
+    // Make navigation menu fixed if scrolled past header
+    $(window).scroll(function() {
+        var windowTop = $(window).scrollTop();
+        var headerHeight = $("header").height();
+        if (windowTop > headerHeight) {
+            $("nav").addClass("fixed-header");
+        }
+        else {
+            $("nav").removeClass("fixed-header");
+        }
+    });
+
+    // Pull out mobile menu when menu button is clicked
+    $(".hamburger").click(function() {
+        $(".menu-list").toggleClass("active");
+    });
+
+    // Close mobile menu when item is clicked
+    $(".menu-button").click(function() {
+        $(".menu-list").removeClass("active");
+    });
+
+    // Make project image "hover" when text is hovered
+    $(".project-text").hover(function() {
+        $(this).prev().toggleClass("hoverImage");
+    });
+
+    // Clicking on project image should follow layer'd text's href
+    $(".project-image").click(function() {
+        var projectLink = $(this).next();
+        window.open(projectLink.attr("href"));
+    });
+
+});
