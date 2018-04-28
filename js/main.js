a96212e13828397cb236389760008e55cc6096b3
FikoReborn <fikosan@gmail.com>
2018-04-27 20:55:49 -0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -11,9 +11,29 @@ $(document).ready(function() {
             $("nav").removeClass("fixed-header");
         }
     });
+    
+    // Change menu button class to "active" depending on where user is located on the page
+    $(window).scroll(function() {
+        var windowTop = $(window).scrollTop();
+        var windowHeight = $(window).height();
+        var documentHeight = $(document).height();
+        var navHeight = $("nav").height();
+        var bottomOfPage = windowHeight + windowTop;
+        var aboutLoc = $(".about-section").offset();
+        var projectsLoc = $(".projects").offset();
+        var contactLoc = $(".contact").offset();
+        console.log(documentHeight);
+        if (windowTop >= 0 && windowTop < projectsLoc.top - 100) {
+            $("#about-button").addClass("menu-active").siblings().removeClass("menu-active");
+        } else if (bottomOfPage > documentHeight - 100) {
+            $("#contact-button").addClass("menu-active").siblings().removeClass("menu-active");
+        } else if (windowTop >= projectsLoc.top && windowTop < contactLoc.top) {
+            $("#projects-button").addClass("menu-active").siblings().removeClass("menu-active");
+        }
+    });
 
     // Pull out mobile menu when menu button is clicked
-    $(".hamburger").click(function() {
+    $(".mobile-menu").click(function() {
         $(".menu-list").toggleClass("active");
     });
 
@@ -27,7 +47,7 @@ $(document).ready(function() {
         $(this).prev().toggleClass("hoverImage");
     });
 
-    // Clicking on project image should follow layer'd text's href
+    // Clicking on project image should follow layered text's href
     $(".project-image").click(function() {
         var projectLink = $(this).next();
         window.open(projectLink.attr("href"));
